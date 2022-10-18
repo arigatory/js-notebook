af25f9ec8a14928762abfdcd8158d6c581f35263
Ivan MSI <arigatory@gmail.com>
2022-10-18 22:12:31 +0300

    feat: types around events

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import Parent from "./props/Parent";
+import UserSearch from "./refs/UserSearch";
  
 const el = document.getElementById("root");
  
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <Parent/>
+      <UserSearch/>
     </div>
   );
 };
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,10 @@
-import { Child } from './Child';
+import { ChildAsFC } from './Child';
 
-const Parent = () => { 
-  return <Child color='red blue green' />
+const Parent = () => {
+  return <ChildAsFC color="red blue green"
+    onClick={() => console.log('hi')}>
+    <h1>hihi</h1>
+  </ChildAsFC>;
 };
 
 export default Parent;
